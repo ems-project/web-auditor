97426b1df671b160bbe436ec3385fc72b10cb671
Mathieu De Keyzer <me@theus.be>
2024-01-04 09:33:44 +0000

    fix: catch all in requestHandler

--- src/audit.js
+++ src/audit.js
@@ -24,70 +24,74 @@ const crawler = new PuppeteerCrawler({
         },
     ],
     async requestHandler({ request, page, enqueueLinks, log }) {
-        const metaTitle = await page.title();
-        const title = await page.$('h1') ? await page.$eval('h1', el => el.textContent) : null
-        const locale = await page.$('html') ? await page.$eval('html', el => el.getAttribute('lang')) : null
+        try {
+            const metaTitle = await page.title();
+            const title = await page.$('h1') ? await page.$eval('h1', el => el.textContent) : null
+            const locale = await page.$('html') ? await page.$eval('html', el => el.getAttribute('lang')) : null
 
-        const response = page.waitForResponse(request.loadedUrl)
-        const audit = await pa11y(request.loadedUrl, {
-            browser: page.browser(),
-            page: page,
-        });
+            const response = page.waitForResponse(request.loadedUrl)
+            const audit = await pa11y(request.loadedUrl, {
+                browser: page.browser(),
+                page: page,
+            });
 
-        const headers = (await response).headers();
-        if (audit.issues.length > 0) {
-            log.info(`${audit.issues.length} errors with page ${request.loadedUrl} : '${metaTitle}'`);
+            const headers = (await response).headers();
+            if (audit.issues.length > 0) {
+                log.info(`${audit.issues.length} errors with page ${request.loadedUrl} : '${metaTitle}'`);
+            }
+
+            const url = new URL(request.loadedUrl)
+            let status = (await response).status()
+            if (304 === status) {
+                status = 200
+            }
+            // Save results as JSON to ./storage/datasets/default
+            await Dataset.pushData({
+                title: title,
+                meta_title: metaTitle,
+                url: request.loadedUrl,
+                pa11y: audit.issues,
+                host: url.hostname,
+                base_url: url.pathname,
+                status_code: status,
+                mimetype: headers['content-type'],
+                locale: locale,
+                size: headers['content-length'],
+                timestamp: String.getTimestamp(),
+            });
+            await enqueueLinks({
+                transformRequestFunction(req) {
+                    const url = new URL(req.url)
+
+                    if (url.pathname.endsWith('.pdf')) return false
+                    if (url.pathname.endsWith('.doc')) return false
+                    if (url.pathname.endsWith('.docx')) return false
+                    if (url.pathname.endsWith('.xls')) return false
+                    if (url.pathname.endsWith('.xlsx')) return false
+                    if (url.pathname.endsWith('.zip')) return false
+                    if (url.pathname.endsWith('.xlsm')) return false
+                    if (url.pathname.endsWith('.xml')) return false
+                    if (url.pathname.endsWith('.odt')) return false
+                    if (url.pathname.endsWith('.dwg')) return false
+
+                    const shasum = crypto.createHash('sha1')
+                    shasum.update(`AuditHashSeed$${url.origin}${url.pathname}`)
+                    const hash = shasum.digest('hex')
+                    if (hashes.includes(hash)) {
+                        return false
+                    }
+                    hashes.push(hash)
+
+                    return req
+                },
+            });
+            this.requestQueue.getInfo().then((info) => {
+                progressBar.update(info.handledRequestCount)
+                progressBar.setTotal(info.totalRequestCount)
+            })
+        } catch (err) {
+            console.log(err)
         }
-
-        const url = new URL(request.loadedUrl)
-        let status = (await response).status()
-        if (304 === status) {
-            status = 200
-        }
-        // Save results as JSON to ./storage/datasets/default
-        await Dataset.pushData({
-            title: title,
-            meta_title: metaTitle,
-            url: request.loadedUrl,
-            pa11y: audit.issues,
-            host: url.hostname,
-            base_url: url.pathname,
-            status_code: status,
-            mimetype: headers['content-type'],
-            locale: locale,
-            size: headers['content-length'],
-            timestamp: String.getTimestamp(),
-        });
-        await enqueueLinks({
-            transformRequestFunction(req) {
-                const url = new URL(req.url)
-
-                if (url.pathname.endsWith('.pdf')) return false
-                if (url.pathname.endsWith('.doc')) return false
-                if (url.pathname.endsWith('.docx')) return false
-                if (url.pathname.endsWith('.xls')) return false
-                if (url.pathname.endsWith('.xlsx')) return false
-                if (url.pathname.endsWith('.zip')) return false
-                if (url.pathname.endsWith('.xlsm')) return false
-                if (url.pathname.endsWith('.xml')) return false
-                if (url.pathname.endsWith('.odt')) return false
-                if (url.pathname.endsWith('.dwg')) return false
-
-                const shasum = crypto.createHash('sha1')
-                shasum.update(`AuditHashSeed$${url.origin}${url.pathname}`)
-                const hash = shasum.digest('hex')
-                if (hashes.includes(hash)) {
-                    return false
-                }
-                hashes.push(hash)
-
-                return req
-            },
-        });
-        this.requestQueue.getInfo().then((info) => {
-            progressBar.update(info.handledRequestCount)
-            progressBar.setTotal(info.totalRequestCount)
-        })
     },
     async failedRequestHandler({ request }) {
         console.log(request)
