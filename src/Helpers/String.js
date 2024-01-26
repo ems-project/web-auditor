c8c6e1156a88f3707614e7d1315124ff00f95189
Mathieu De Keyzer <me@theus.be>
2024-01-26 13:39:51 +0100

    fix: camelcase

--- src/Helpers/String.js
+++ src/Helpers/String.js
@@ -1,28 +1,28 @@
 "use strict";
 
-module.exports.getTimestamp = function getTimestamp() {
+module.exports.getTimestamp = function getTimestamp () {
     const now = new Date()
-    const timezone_offset_min = now.getTimezoneOffset()
-    let offset_hrs = parseInt(Math.abs(timezone_offset_min / 60))
-    let offset_min = Math.abs(timezone_offset_min % 60)
-    let timezone_standard
+    const timezoneOffset = now.getTimezoneOffset()
+    let offsetHours = parseInt(Math.abs(timezoneOffset / 60))
+    let offsetMin = Math.abs(timezoneOffset % 60)
+    let timezoneStandard
 
-    if (offset_hrs < 10) {
-        offset_hrs = '0' + offset_hrs;
+    if (offsetHours < 10) {
+        offsetHours = '0' + offsetHours
     }
-    if (offset_min < 10) {
-        offset_min = '0' + offset_min;
+    if (offsetMin < 10) {
+        offsetMin = '0' + offsetMin
     }
 
-    if (timezone_offset_min < 0) {
-        timezone_standard = '+' + offset_hrs + ':' + offset_min
+    if (timezoneOffset < 0) {
+        timezoneStandard = '+' + offsetHours + ':' + offsetMin
     }
-    else if (timezone_offset_min > 0) {
-        timezone_standard = '-' + offset_hrs + ':' + offset_min
+    else if (timezoneOffset > 0) {
+        timezoneStandard = '-' + offsetHours + ':' + offsetMin
     }
     else {
-        timezone_standard = 'Z'
+        timezoneStandard = 'Z'
     }
 
-    return now.toISOString().slice(0, 19) + timezone_standard
+  return now.toISOString().slice(0, 19) + timezoneStandard
 }
