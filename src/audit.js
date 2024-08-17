31de9831008d28e2ba69738b23c7f0e6e1f05b88
Mathieu De Keyzer <me@theus.be>
2024-08-17 11:01:11 +0200

    ref: rollback on statusByUrl

--- src/audit.js
+++ src/audit.js
@@ -13,50 +13,6 @@ const hashes = []
 const referers = []
 let dataset = null
 const linkAuditor = new LinkAuditor(ca)
-const statusByUrl = []
-const friendlyHttpStatus = {
-  200: 'OK',
-  201: 'Created',
-  202: 'Accepted',
-  203: 'Non-Authoritative Information',
-  204: 'No Content',
-  205: 'Reset Content',
-  206: 'Partial Content',
-  300: 'Multiple Choices',
-  301: 'Moved Permanently',
-  302: 'Found',
-  303: 'See Other',
-  304: 'Not Modified',
-  305: 'Use Proxy',
-  306: 'Unused',
-  307: 'Temporary Redirect',
-  400: 'Bad Request',
-  401: 'Unauthorized',
-  402: 'Payment Required',
-  403: 'Forbidden',
-  404: 'Not Found',
-  405: 'Method Not Allowed',
-  406: 'Not Acceptable',
-  407: 'Proxy Authentication Required',
-  408: 'Request Timeout',
-  409: 'Conflict',
-  410: 'Gone',
-  411: 'Length Required',
-  412: 'Precondition Required',
-  413: 'Request Entry Too Large',
-  414: 'Request-URI Too Long',
-  415: 'Unsupported Media Type',
-  416: 'Requested Range Not Satisfiable',
-  417: 'Expectation Failed',
-  418: 'I\'m a teapot',
-  429: 'Too Many Requests',
-  500: 'Internal Server Error',
-  501: 'Not Implemented',
-  502: 'Bad Gateway',
-  503: 'Service Unavailable',
-  504: 'Gateway Timeout',
-  505: 'HTTP Version Not Supported'
-}
 
 let pagesWithIssuesCount = 0
 let totalIssuesCount = 0
@@ -178,10 +134,6 @@ const crawler = new PuppeteerCrawler({
       data.error = err.message ?? 'This url encountered an unknown error'
     } finally {
       await dataset.pushData(data)
-      statusByUrl[data.url] = {
-        status_code: data.status_code,
-        message: data.error ?? friendlyHttpStatus[data.status_code] ?? ''
-      }
     }
     await enqueueLinks({
       transformRequestFunction (req) {
@@ -219,10 +171,6 @@ const crawler = new PuppeteerCrawler({
       status_code: curlStatus[0].status_code ?? 500
     }
     await dataset.pushData(data)
-    statusByUrl[request.url] = {
-      status_code: data.status_code,
-      message: curlStatus[0].statusMessage ?? friendlyHttpStatus[data.status_code] ?? ''
-    }
   },
   headless: true,
   maxConcurrency: 100
