b98579be7099e135c3bd105259eca47ea96f19df
Mathieu De Keyzer <me@theus.be>
2024-08-20 09:26:44 +0200

    fix: only keep 404 and above

--- src/upload.js
+++ src/upload.js
@@ -38,7 +38,7 @@ const Process = require('./Helpers/Process');
       })
     }
     if (document.links && Array.isArray(document.links)) {
-      document.links = document.links.filter(link => link.status_code >= 404)
+      document.links = document.links.filter(link => link.status_code >= 404).slice(0, 200)
     }
     const url = new URL(document.url)
     const sha1Sum = crypto.createHash('sha1')
